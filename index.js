bbc0034977ef85e5ac29bc2de3322f65ddcd7291
DESKTOP-J7FEJO4\Admin <wc.jaylimbasiya@gmail.com>
2025-05-13 09:10:11 +0530

    $title : bug fix in github integratoin $description : we have optimize the functionality and bug fixed and improvment $label: bug fixed $title : bug fix in github integratoin we have optimize the functionality and bug fixed and improvment bug fixed

--- index.js
+++ index.js
@@ -6,3 +6,13 @@
 //new commit 
 //github commit in specific formate
 //$title : bug fix in github integratoin $description : we have optimize the functionality and bug fixed and improvment $label: bug fixed 
+//console.log("push event called", payload.commits[0].message);
+
+// First, split by $title :
+const [, titleAndRest] = payload.commits[0].message.split("$title :");
+const [title, descAndLabel] = titleAndRest.split("$description :");
+const [description, label] = descAndLabel.split("$label:");
+
+console.log(title.trim());
+console.log(description.trim());
+console.log(label.trim());
