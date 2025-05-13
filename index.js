01d8190ef839d7e5bb126f9b41ee69c7cfd83b53
DESKTOP-J7FEJO4\Admin <wc.jaylimbasiya@gmail.com>
2025-05-13 10:42:54 +0530

    $title : Github integartion  $description : we have launch new feature github integration you can now connect your github repo and manage commits amd issues directly by integrate github $label: new feature

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ const [description, label] = descAndLabel.split("$label:");
 console.log(title.trim());
 console.log(description.trim());
 console.log(label.trim());    
-//new commit        
+//new commit         
 //label founds        
